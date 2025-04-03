9a3724cf927100a92f397957d61751715e05c02d
rupam <rexrupam70@gmail.com>
2025-04-03 16:42:21 +0530

    add comment related api like add comment

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from "express"
 import cors from "cors"
 import userRoute from "../src/routes/user.routes.js"
 import videoRoute from "../src/routes/video.routes.js"
+import commentRoute from "../src/routes/comment.routes.js"
 export const app = express();
+
 
 
 app.use(cors({
@@ -16,3 +18,4 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use('/users', userRoute)
 app.use('/video', videoRoute)
+app.use('/comment', commentRoute)
