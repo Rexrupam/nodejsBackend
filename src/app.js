6b94d51ed3f684a670c3ce95912391bef82e12a1
rupam <rexrupam70@gmail.com>
2025-04-03 16:27:42 +0530

    fixes some bug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
+import userRoute from "../src/routes/user.routes.js"
+import videoRoute from "../src/routes/video.routes.js"
 export const app = express();
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -11,3 +14,5 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
+app.use('/users', userRoute)
+app.use('/video', videoRoute)
